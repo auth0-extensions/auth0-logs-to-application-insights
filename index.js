0f061b7b2036370a42e2369efb8920d75ee074da
jcenturion <javier.centurion@auth0.com>
2016-02-23 17:22:20 -0300

    Update checkpoint calculations

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 "use latest";
 
-const Auth0     = require('auth0@0.8.2');
+const Auth0     = require('auth0');
 const useragent = require('useragent');
 const moment    = require('moment');
+const express   = require('express');
+const Webtask   = require('webtask-tools');
+const app       = express();
 
 /*
  * Get the application insights client.
@@ -45,212 +48,201 @@ const getClient = (key) => {
   return client;
 };
 
-module.exports = (ctx, done) => {
+function lastLogCheckpoint (req, res) {
+  let ctx = req.webtaskContext;
+
   if (!ctx.data.AUTH0_DOMAIN || !ctx.data.AUTH0_GLOBAL_CLIENT_ID || !ctx.data.AUTH0_GLOBAL_CLIENT_SECRET) {
-    return done({ message: 'Auth0 API v1 credentials or domain missing.' });
+    return res.status(400).send({ message: 'Auth0 API v1 credentials or domain missing.' });
   }
 
   if (!ctx.data.APPINSIGHTS_INSTRUMENTATIONKEY) {
-    return done({ message: 'Application Insights instrumentation key is missing.' });
+    return res.status(400).send({ message: 'Application Insights instrumentation key is missing.' });
   }
 
-  /*
-   * If this is a scheduled task, we'll get the last log checkpoint from the previous run and continue from there.
-   */
-  let checkpointId = null;
-  let startCheckpointId = null;
-  if (ctx.body && ctx.body.results && ctx.body.results.length > 0) {
-    console.log('Trying to get last checkpointId from previous run.');
+  req.webtaskContext.read('history', {}, function (err, data) {
+    let checkpointId = typeof data === 'undefined' ? null : data.checkpointId;
+    /*
+     * If this is a scheduled task, we'll get the last log checkpoint from the previous run and continue from there.
+     */
+    console.log('Starting from:', checkpointId);
 
-    for (var i = 0; i < ctx.body.results.length; i++) {
-      console.log (' > Run:', ctx.body.results[i]);
-      if (ctx.body.results[i].statusCode === 200 && ctx.body.results[i].body){
-        var result = JSON.parse(ctx.body.results[i].body);
-        if (result && result.checkpointId) {
-          checkpointId = result.checkpointId;
-          startCheckpointId = result.checkpointId;
+    const client = getClient(ctx.data.APPINSIGHTS_INSTRUMENTATIONKEY);
+    client.commonProperties = {
+      auth0_domain: ctx.data.AUTH0_DOMAIN
+    };
 
-          console.log (' > This is the last one we want to continue from.');
-          break;
+    const auth0 = new Auth0({
+      domain:       ctx.data.AUTH0_DOMAIN,
+      clientID:     ctx.data.AUTH0_GLOBAL_CLIENT_ID,
+      clientSecret: ctx.data.AUTH0_GLOBAL_CLIENT_SECRET
+    });
+
+    /*
+     * Test authenticating with the Auth0 API.
+     */
+    const authenticate = (callback) => {
+      auth0.getAccessToken(function (err, newToken) {
+        console.log('Authenticating...');
+
+        if (err) {
+          console.log('Error authenticating', err);
+          return callback(err);
         }
+
+        console.log('Authentication success.');
+        return callback();
+      });
+    };
+
+    /*
+     * Get the logs from Auth0.
+     */
+    const logs = [];
+    const getLogs = (checkPoint, callback) => {
+      auth0.getLogs({ take: 200, from: checkPoint }, (err, result) => {
+        if (err) {
+          return console.log('Error getting logs:', err.message);
+        }
+
+        if (result && result.length > 0) {
+          result.forEach((log) => {
+            // Application Insights does not allow you to send very old logs, so we'll only send the logs of the last 48 hours max.
+            if (log.date && moment().diff(moment(log.date), 'hours') < 48) {
+              logs.push(log);
+            }
+          });
+
+          console.log(`Retrieved ${logs.length} logs from Auth0 after ${checkPoint}.`);
+          setImmediate(() => {
+            checkpointId = result[result.length - 1]._id;
+            getLogs(result[result.length - 1]._id, callback);
+          });
+        }
+        else {
+          console.log(`Reached end of logs. Total: ${logs.length}.`);
+          return callback(null, logs);
+        }
+      });
+    };
+
+    /*
+     * Export the logs to Application Insights.
+     */
+    const exportLogs = (logs, callback) => {
+      console.log('Exporting logs to Application Insights: ' + logs.length);
+
+      logs.forEach(function(record) {
+        var level = 0;
+        record.type_code = record.type;
+        if (logTypes[record.type]) {
+          level = logTypes[record.type].level;
+          record.type = logTypes[record.type].event;
+        }
+
+        // Application Insights does not like null or empty strings.
+        if (!record.ip || record.ip === '') delete record.ip;
+        if (!record.user_id || record.user_id === '')  delete record.user_id;
+        if (!record.user_name || record.user_name === '')  delete record.user_name;
+        if (!record.connection || record.connection === '')  delete record.connection;
+        if (!record.client_name || record.client_name === '') delete record.client_name;
+        if (!record.description || record.description === '')  delete record.description;
+
+        // Application Insights does not like booleans.
+        record.isMobile = record.isMobile && 'yes' || 'no';
+
+        // Application Insights does not like objects.
+        if (record.details) {
+          record.details = JSON.stringify(record.details, null, 2);
+        }
+
+        // Application Insights does not like login strings.
+        if (record.details && record.details.length > 8185) {
+          record.details = record.details.substring(0, 8185) + '...';
+        }
+
+        var agent = useragent.parse(record.user_agent);
+        record.os = agent.os.toString();
+        record.os_version = agent.os.toVersion();
+        record.device = agent.device.toString();
+        record.device_version = agent.device.toVersion();
+
+        // Don't show "Generic Smartphone" in Application Insightis.
+        if (record.device && record.device.indexOf('Generic Smartphone') >= 0) {
+          record.device = agent.os.toString();
+          record.device_version = agent.os.toVersion();
+        }
+
+        if (level >= 3) {
+          var error = new Error(record.type);
+          error.name = record.type;
+          client.trackException(error, record);
+        }
+
+        client.trackEvent(record.type, record);
+      });
+
+      if (logs && logs.length) {
+        console.log('Flushing all data...');
+
+        client.sendPendingData((response) => {
+          return callback(null, response);
+        });
+      } else {
+        console.log('No data to flush...');
+
+        return callback(null, '{ "itemsAccepted": 0 }');
       }
     };
 
-    console.log('Starting from:', checkpointId);
-  }
-
-  const client = getClient(ctx.data.APPINSIGHTS_INSTRUMENTATIONKEY);
-  client.commonProperties = {
-    auth0_domain: ctx.data.AUTH0_DOMAIN
-  };
-
-  const auth0 = new Auth0({
-    domain: ctx.data.AUTH0_DOMAIN,
-    clientID: ctx.data.AUTH0_GLOBAL_CLIENT_ID,
-    clientSecret: ctx.data.AUTH0_GLOBAL_CLIENT_SECRET
-  });
-
-  /*
-   * Test authenticating with the Auth0 API.
-   */
-  const authenticate = (callback) => {
-    auth0.getAccessToken(function (err, newToken) {
-      console.log('Authenticating...');
-
+    /*
+     * Start the process.
+     */
+    authenticate((err) => {
       if (err) {
-        console.log('Error authenticating', err);
-        return callback(err);
+        return res.status(500).send({ err: err });
       }
 
-      console.log('Authentication success.');
-      return callback();
-    });
-  };
+      getLogs(checkpointId, (err, logs) => {
+        if (!logs) {
+          return res.status(500).send({ err: err });
+        }
 
-  /*
-   * Get the logs from Auth0.
-   */
-  const logs = [];
-  const getLogs = (checkPoint, callback) => {
-    auth0.getLogs({ take: 200, from: checkPoint }, (err, result) => {
-      if (err) {
-        return console.log('Error getting logs:', err.message);
-      }
+        exportLogs(logs, (err, response) => {
+          try {
+            response = JSON.parse(response);
+          } catch (e) {
+            console.log('Error parsing response, this might indicate that an error occurred:', response);
 
-      if (result && result.length > 0) {
-        result.forEach((log) => {
-          // Application Insights does not allow you to send very old logs, so we'll only send the logs of the last 48 hours max.
-          if (log.date && moment().diff(moment(log.date), 'hours') < 48) {
-            logs.push(log);
+            return req.webtaskContext.write('history', JSON.stringify({checkpointId: checkpointId}), {}, function (error) {
+              if (error) return res.status(500).send(error);
+
+              res.status(500).send({
+                error: response
+              });
+            });
           }
-        });
 
-        console.log(`Retrieved ${logs.length} logs from Auth0 after ${checkPoint}.`);
-        setImmediate(() => {
-          checkpointId = result[result.length - 1]._id;
-          getLogs(result[result.length - 1]._id, callback);
-        });
-      }
-      else {
-        console.log(`Reached end of logs. Total: ${logs.length}.`);
-        return callback(null, logs);
-      }
-    });
-  };
+          // At least one item we sent was accepted, so we're good and next run can continue where we stopped.
+          if (response.itemsAccepted && response.itemsAccepted > 0) {
+            return req.webtaskContext.write('history', JSON.stringify({checkpointId: checkpointId}), {}, function (error) {
+              if (error) return res.status(500).send(error);
 
-  /*
-   * Export the logs to Application Insights.
-   */
-  const exportLogs = (logs, callback) => {
-    console.log('Exporting logs to Application Insights: ' + logs.length);
+              res.sendStatus(200);
+            });
+          }
 
-    logs.forEach(function(record) {
-      var level = 0;
-      record.type_code = record.type;
-      if (logTypes[record.type]) {
-        level = logTypes[record.type].level;
-        record.type = logTypes[record.type].event;
-      }
+          // None of our items were accepted, next run should continue from same starting point.
+          console.log('No items accepted.');
+          return req.webtaskContext.write('history', JSON.stringify({checkpointId: checkpointId}), {}, function (error) {
+            if (error) return res.status(500).send(error);
 
-      // Application Insights does not like null or empty strings.
-      if (!record.ip || record.ip === '') delete record.ip;
-      if (!record.user_id || record.user_id === '')  delete record.user_id;
-      if (!record.user_name || record.user_name === '')  delete record.user_name;
-      if (!record.connection || record.connection === '')  delete record.connection;
-      if (!record.client_name || record.client_name === '') delete record.client_name;
-      if (!record.description || record.description === '')  delete record.description;
-
-      // Application Insights does not like booleans.
-      record.isMobile = record.isMobile && 'yes' || 'no';
-
-      // Application Insights does not like objects.
-      if (record.details) {
-        record.details = JSON.stringify(record.details, null, 2);
-      }
-
-      // Application Insights does not like login strings.
-      if (record.details && record.details.length > 8185) {
-        record.details = record.details.substring(0, 8185) + '...';
-      }
-
-      var agent = useragent.parse(record.user_agent);
-      record.os = agent.os.toString();
-      record.os_version = agent.os.toVersion();
-      record.device = agent.device.toString();
-      record.device_version = agent.device.toVersion();
-
-      // Don't show "Generic Smartphone" in Application Insightis.
-      if (record.device && record.device.indexOf('Generic Smartphone') >= 0) {
-        record.device = agent.os.toString();
-        record.device_version = agent.os.toVersion();
-      }
-
-      if (level >= 3) {
-        var error = new Error(record.type);
-        error.name = record.type;
-        client.trackException(error, record);
-      }
-
-      client.trackEvent(record.type, record);
-    });
-
-    if (logs && logs.length) {
-      console.log('Flushing all data...');
-
-      client.sendPendingData((response) => {
-        return callback(null, response);
-      });
-    } else {
-      console.log('No data to flush...');
-
-      return callback(null, '{ "itemsAccepted": 0 }');
-    }
-  };
-
-  /*
-   * Start the process.
-   */
-  authenticate((err) => {
-    if (err) {
-      return done(err);
-    }
-
-    getLogs(checkpointId, (err, logs) => {
-      if (!logs) {
-        return done(err);
-      }
-
-      exportLogs(logs, (err, response) => {
-        try {
-          response = JSON.parse(response);
-        } catch (e) {
-          console.log('Error parsing response, this might indicate that an error occurred:', response);
-          return done({ error: response }, {
-            startCheckpointId,
-            checkpointId: startCheckpointId
+            res.sendStatus(200);
           });
-        }
-
-        // At least one item we sent was accepted, so we're good and next run can continue where we stopped.
-        if (response.itemsAccepted && response.itemsAccepted > 0) {
-          console.log('Items accepted (Errors/Events):', response.itemsAccepted);
-          return done(null, {
-            startCheckpointId,
-            checkpointId,
-            ...response
-          });
-        }
-
-        // None of our items were accepted, next run should continue from same starting point.
-        console.log('No items accepted.');
-        return done(null, {
-          checkpointId: startCheckpointId,
-          ...response
         });
       });
     });
   });
-};
+}
 
 const logTypes = {
   's': {
@@ -408,3 +400,8 @@ const logTypes = {
     level: 3 // Error
   }
 };
+
+app.get ('/', lastLogCheckpoint);
+app.post('/', lastLogCheckpoint);
+
+module.exports = Webtask.fromExpress(app);
